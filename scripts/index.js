25d797e3c33bbb0bbdfcf68c3064003fafca1569
face_detection <aaryan1@mail.sfsu.edu>
2021-12-05 13:34:58 -0800

    Video 15

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 // DOM elements
-const aboutList = document.querySelector(".about");
+const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const accountDetails = document.querySelector('.account-details');
@@ -7,68 +7,55 @@ const accountDetails = document.querySelector('.account-details');
 const setupUI = (user) => {
   if (user) {
     // account info
-    const html = `
-      <div>Logged in as ${user.email}</div>
-    `;
-    accountDetails.innerHTML = html;
-    // toggle UI elements
+    db.collection('users').doc(user.uid).get().then(doc => {
+      const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+      `;
+      accountDetails.innerHTML = html;
+    });
+    // toggle user UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
     loggedOutLinks.forEach(item => item.style.display = 'none');
   } else {
-    // hide account info
+    // clear account info
     accountDetails.innerHTML = '';
-    // toggle UI elements
+    // toggle user elements
     loggedInLinks.forEach(item => item.style.display = 'none');
     loggedOutLinks.forEach(item => item.style.display = 'block');
-
   }
-}
-
-// setup about
-const setupAbout = (data) => {
-
-    if(data.length) {
-      let html = "";
-      data.forEach((doc) => {
-          const about = doc.data();
-          const li = `
-        <li>
-          <div class="collapsible-header grey lighten-4"> ${about.name} </div>
-          <div class="collapsible-body white"> ${about.email}, ${about.phone}, ${about.city}</div>
-        </li>
-      `;
-          html += li;
-      });
-      aboutList.innerHTML = html;
-    } else {
-      aboutList.innerHTML = '<h5 class="center-align">Login to view Info</h5>'
-    }
 };
 
-// const setupAbout = (data) => {
-//     if (data.length) {
-//         let html = "";
-//         data.forEach((doc) => {
-//             const about = doc.data();
-//             const li = `
-//             <li>
-//             <div class="collapsible-header grey lighten-4"> ${about.name} </div>
-//             <div class="collapsible-body white"> ${about.email}, ${about.phone}, ${about.city}</div>
-//           </li>
-//         `;
-//             html += li;
-//         });
-//         aboutList.innerHTML = html;
-//     } else {
-//         aboutList.innerHTML = '<h5 class="center-align">Login to view about me Cards</h5>';
-//     }
-// };
+// setup guides
+const setupGuides = (data) => {
+
+  if (data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html
+  } else {
+    guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
+  }
+  
+
+};
 
 // setup materialize components
-document.addEventListener("DOMContentLoaded", function () {
-    var modals = document.querySelectorAll(".modal");
-    M.Modal.init(modals);
+document.addEventListener('DOMContentLoaded', function() {
 
-    var items = document.querySelectorAll(".collapsible");
-    M.Collapsible.init(items);
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
 });
