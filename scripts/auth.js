4365b65deaacbe1b4df87a91ce1d4ed007d34b8e
Anmol Burmy <anmolburmy145@gmail.com>
2021-12-06 18:35:02 -0800

    added errors

--- scripts/auth.js
+++ scripts/auth.js
@@ -87,6 +87,10 @@ signupForm.addEventListener("submit", (e) => {
             const modal = document.querySelector("#modal-signup");
             M.Modal.getInstance(modal).close();
             signupForm.reset();
+            signupForm.querySelector(".error").innerHTML = "";
+        })
+        .catch((err) => {
+            signupForm.querySelector(".error").innerHTML = err.message;
         });
 });
 
@@ -107,10 +111,15 @@ loginForm.addEventListener("submit", (e) => {
     const password = loginForm["login-password"].value;
 
     // log the user in
-    auth.signInWithEmailAndPassword(email, password).then((cred) => {
-        // close the signup modal & reset form
-        const modal = document.querySelector("#modal-login");
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-    });
+    auth.signInWithEmailAndPassword(email, password)
+        .then((cred) => {
+            // close the signup modal & reset form
+            const modal = document.querySelector("#modal-login");
+            M.Modal.getInstance(modal).close();
+            loginForm.reset();
+            loginForm.querySelector(".error").innerHTML = "";
+        })
+        .catch((err) => {
+            loginForm.querySelector(".error").innerHTML = err.message;
+        });
 });
--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@
 const aboutList = document.querySelector(".about");
 const loggedOutLinks = document.querySelectorAll(".logged-out");
 const loggedInLinks = document.querySelectorAll(".logged-in");
-const accountDetails = document.querySelector(".account-details");
 const adminItems = document.querySelectorAll(".admin");
 const setupUI = (user) => {
     if (user) {
@@ -19,7 +18,6 @@ const setupUI = (user) => {
       <div>${doc.data().bio}</div>
       <div class="pink-text">${user.admin ? "Admin" : ""}</div>
     `;
-                accountDetails.innerHTML = html;
             });
 
         // toggle UI elements
@@ -27,7 +25,7 @@ const setupUI = (user) => {
         loggedOutLinks.forEach((item) => (item.style.display = "none"));
     } else {
         // hide account info
-        accountDetails.innerHTML = "";
+
         // toggle UI elements
         adminItems.forEach((item) => (item.style.display = "none"));
         loggedInLinks.forEach((item) => (item.style.display = "none"));
@@ -45,6 +43,7 @@ const setupAbout = (data) => {
         <li>
           <div class="collapsible-header grey lighten-4"> ${about.name} </div>
           <div class="collapsible-body white"> ${about.email}, ${about.phone}, ${about.city}</div>
+
         </li>
       `;
             html += li;
@@ -54,25 +53,6 @@ const setupAbout = (data) => {
         aboutList.innerHTML = '<h5 class="center-align">Login to view Info</h5>';
     }
 };
-
-// const setupAbout = (data) => {
-//     if (data.length) {
-//         let html = "";
-//         data.forEach((doc) => {
-//             const about = doc.data();
-//             const li = `
-//             <li>
-//             <div class="collapsible-header grey lighten-4"> ${about.name} </div>
-//             <div class="collapsible-body white"> ${about.email}, ${about.phone}, ${about.city}</div>
-//           </li>
-//         `;
-//             html += li;
-//         });
-//         aboutList.innerHTML = html;
-//     } else {
-//         aboutList.innerHTML = '<h5 class="center-align">Login to view about me Cards</h5>';
-//     }
-// };
 
 // setup materialize components
 document.addEventListener("DOMContentLoaded", function () {
